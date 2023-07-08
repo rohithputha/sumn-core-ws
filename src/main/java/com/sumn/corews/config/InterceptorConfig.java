662021726bf6300d0793b7cbe71708c26e3da236
Rohith <rohith@192.168.1.5>
2023-07-08 15:47:09 +0530

    reverting API security changes

--- src/main/java/com/sumn/corews/config/InterceptorConfig.java
+++ src/main/java/com/sumn/corews/config/InterceptorConfig.java
@@ -1,7 +1,7 @@
 package com.sumn.corews.config;
 
 import com.google.api.client.util.Value;
-import com.sumn.corews.middleware.ApiAuthentication;
+//import com.sumn.corews.middleware.ApiAuthentication;
 import com.sumn.corews.middleware.UserRetrieve;
 import com.sumn.corews.dao.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,16 +14,16 @@ public class InterceptorConfig implements WebMvcConfigurer {
 
     private UserDAO userDAO;
 
-    private AppConfig appConfig;
+//    private AppConfig appConfig;
 
-    public InterceptorConfig(UserDAO userDAO, AppConfig appConfig){
+    public InterceptorConfig(UserDAO userDAO){
         this.userDAO = userDAO;
-        this.appConfig = appConfig;
+//        this.appConfig = appConfig;
     }
 
     public void addInterceptors(InterceptorRegistry registry){
 
-        registry.addInterceptor(new ApiAuthentication(appConfig)).addPathPatterns("/**");
+//        registry.addInterceptor(new ApiAuthentication(appConfig)).addPathPatterns("/**");
         registry.addInterceptor(new UserRetrieve(userDAO)).addPathPatterns("/**");
     }
 }
